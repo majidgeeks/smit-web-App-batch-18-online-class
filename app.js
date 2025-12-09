e5b320d1e864d74ba1a28206736f648b1eb780c4
Majid Hussain <majidrajput246@gmail.com>
2025-12-09 19:49:26 +0500

    Js class 2

---
+++ app.js
@@ -0,0 +1,48 @@
+// let age = 18;
+// let hasID = true;
+
+// let isWeekend = false;
+// let isHoliday = true;
+
+// let loggedIn = true; // ya false
+// let age = 10;        // player ki age
+// let guest = false; 
+
+
+// var arr1 = [];
+
+// // document.write(arr1);
+// // console.log(arr1)
+
+// var name1 = 'Salman';
+// arr1[0] = name1;
+
+// // console.log(arr1)
+
+// arr1[5] = 'The Lost World';
+// // console.log(arr1)
+
+// arr1[9] = 10;
+// arr1[1] = undefined;
+// arr1[2] = undefined;
+// arr1[3] = null;
+// arr1[4] = undefined;
+
+
+// console.log(arr1)
+// console.log(arr1.length)
+
+var fruits = ['Banana', 'Mango', 'Apple']; 
+var car = 'Toyota';
+
+// console.log(fruits)
+
+fruits[0] = 'Strawberry';
+console.log(fruits)
+console.log('car',car)
+
+
+
+
+
+
