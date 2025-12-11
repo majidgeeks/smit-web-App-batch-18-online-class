69e948bf1ec84c9ddf21567e66d2abb9903a8048
Majid Hussain <majidrajput246@gmail.com>
2025-12-11 20:00:49 +0500

    js class 2

---
+++ Js/Js class 3/app.js
@@ -0,0 +1,31 @@
+// var cars = ['toyota', 'Honda', 'Suzuki'];
+
+// var others = ['hayabusa', 'lamborghini', 'bughati'];
+
+// console.log('before',cars);
+
+// cars.splice(1,2, others);
+
+// console.log('after',cars);
+
+// var trucks = ['isuzu', 'toyota', 'hyundai', 'mazda'];
+
+// // console.log(cars.length)
+
+// cars.splice(cars.length, 0, trucks);
+
+// console.log('after truck', cars)
+
+
+// slice 
+
+
+var cars = ['toyota', 'Honda', 'Suzuki', 'hyundai', 'mazda'];
+
+console.log('cars',cars)
+
+var newCars = cars.slice(2);
+
+console.log('after',newCars)
+
+
