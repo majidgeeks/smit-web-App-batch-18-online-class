9cb75b3023d386c1f0b2daa7de0f9768a4adf576
Majid Hussain <majidrajput246@gmail.com>
2025-12-20 20:09:24 +0500

    js class loop conditional

--- Js/Js class 4 functions/app.js
+++ Js/Js class 4 functions/app.js
@@ -1,35 +1,43 @@
-// functions 
+// // functions 
 
 
 
-function Calculate(n1, n2, operate) {
-    var total;
-    if(operate === '+'){
-      total = n1 + n2;
-    }else if(operate === '-'){
-      total = n1 - n2;
-    }else if(operate === '*'){
-      total = n1 * n2;
-    }else if(operate === '/'){
-      total = n1 / n2;
-    }else{
-        alert('plz enter right number or operator ');
-    }
+// function Calculate(n1, n2, operate) {
+//     var total;
+//     if(operate === '+'){
+//       total = n1 + n2;
+//     }else if(operate === '-'){
+//       total = n1 - n2;
+//     }else if(operate === '*'){
+//       total = n1 * n2;
+//     }else if(operate === '/'){
+//       total = n1 / n2;
+//     }else{
+//         alert('plz enter right number or operator ');
+//     }
 
-    return total;      
-    };
+//     return total;      
+//     };
 
     
-    var num1 = Number(prompt('enter num1'));
-    var num2 = Number(prompt('enter num2'));
-    var operator = prompt('+, -, *, /');   
+//     var num1 = Number(prompt('enter num1'));
+//     var num2 = Number(prompt('enter num2'));
+//     var operator = prompt('+, -, *, /');   
 
-    var output = Calculate(num1, num2, operator);
+//     var output = Calculate(num1, num2, operator);
 
-    // console.log('output',output);
+//     // console.log('output',output);
 
-    var heading = document.getElementById('h1');
-    // heading.innerHTML = 'i will display';
-    heading.innerText = `calculation is ${output}`;
+//     var heading = document.getElementById('h1');
+//     // heading.innerHTML = 'i will display';
+//     heading.innerText = `calculation is ${output}`;
 
-    // console.log('heading',heading)
+//     // console.log('heading',heading)
+
+
+const div1 = `<div class='div'>
+             <h1>the world</h1>
+             <h1>Here is para</h1>
+             </div>`
+
+      document.write(div1);       
---
+++ Js/Js class 5 loops/app.js
@@ -0,0 +1,41 @@
+// var cities = ['Karachi', 'Islamabad', 'Lahore', 'Multan', 'Murree', 'Swat'];
+
+// var userInput = prompt('enter city name');
+// var isValid = false;
+
+
+// for(var i = 0; i < cities.length; i++){
+//  if(userInput == cities[i]){ 
+//     // alert('you are eligible');
+//     isValid = true;
+//     break;   
+// }
+// // else{
+// //     alert('you are not eligible');
+// //     isValid = false;
+// //     break;
+// // }
+// // else if(userInput !== cities[i]){
+// //     alert('you are not eligible');
+// //     isValid = false;
+// //     break;
+// // }
+// };
+
+// if(!isValid){
+//     alert('you are not eligible');
+// }
+
+// console.log('isValid',isValid)
+
+
+var newUser = 'majid';
+var isLogeIn = false;
+
+if(!isLogeIn){
+   var user = 'hamad';
+   console.log('chal gaya if block')
+}
+
+console.log('newUser',newUser)
+console.log('user',user)
