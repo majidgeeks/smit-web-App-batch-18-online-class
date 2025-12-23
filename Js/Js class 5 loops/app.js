acdd6f9e1c3281d832ef0a38d18c16fd16a9b6d1
Majid Hussain <majidrajput246@gmail.com>
2025-12-23 23:01:36 +0500

    js class online nested loops

--- Js/Js class 5 loops/app.js
+++ Js/Js class 5 loops/app.js
@@ -29,13 +29,48 @@
 // console.log('isValid',isValid)
 
 
-var newUser = 'majid';
-var isLogeIn = false;
+// var newUser = 'majid';
+// var isLogeIn = false;
 
-if(!isLogeIn){
-   var user = 'hamad';
-   console.log('chal gaya if block')
-}
+// if(!isLogeIn){
+//    var user = 'hamad';
+//    console.log('chal gaya if block')
+// }
 
-console.log('newUser',newUser)
-console.log('user',user)
+// console.log('newUser',newUser)
+// console.log('user',user)
+
+
+// var arr1 = ['jazz', 'zong', 'warid'];
+// var arr2 = ['telenor', 'ufone'];
+
+// for(var i = 0; i < arr1.length; i++){
+//    for(var j = 0; j < arr2.length; j++){
+//       document.write(`${arr1[i]} ${arr2[j]} <br>`);
+//    }
+// }
+
+
+// for(var i = 1; i <= 100; i = i + 10){
+//    for(var j = i; j < i + 10; j++){
+//       document.write(j + ' ');
+//    }
+//    document.write('<br>');
+// }
+
+
+// // star pattern 
+
+// for(var i = 1; i <= 5; i++){
+//    for(var j = 1; j <= i; j++){
+//       document.write('*');
+//    }
+//    document.write('<br>');
+// }
+
+// for(var i = 5; i >= 1; i--){
+//    for(var j = 0; j < i; j++){
+//       document.write('*');
+//    }
+//    document.write('<br>')
+// }
---
+++ Js/Js class 6 nested loops/app.js
@@ -0,0 +1,33 @@
+// nested loops
+
+
+// for(var i = 1; i <=100; i = i +10){ // outer loop
+//     for(var j = i; j < i + 10; j++){ // inner loop
+//         document.write(j + ' '); // till 10
+//     }
+//     document.write('<br>');
+// }
+
+
+// star pattern 
+
+// for(var i = 1; i <= 5; i++){ // outer 
+//     for(var j = 1; j <= i; j++){ // inner loop 
+//         document.write('*');
+//     }
+//     document.write('<br>');
+  
+// };
+
+
+
+//  var arr1 = ['jazz', 'zong', 'warid'];
+
+//  var arr2 = ['telenor', 'ufone'];
+
+
+//  for(var i = 0; i < arr1.length; i++){ // outer loop
+//      for(var j = 0; j < arr2.length; j++){
+//         document.write(`${arr1[i]} ${arr2[j]} <br>`)
+//      } // inner loop
+//  }
